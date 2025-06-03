e593d49458d5f82f703408bfd5b50106fe814fc6
Rizki Saepul Aziz <rzkysa0@gmail.com>
2025-06-03 10:14:42 +0700

    feat(devs): update user

--- src/main/java/com/tubes/model/User.java
+++ src/main/java/com/tubes/model/User.java
@@ -8,14 +8,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
     private String username;
+    private String email;
+    private String phone;
     private String password;
 
     public User() {
     }
 
-    public User(String username, String password) {
+    public User(String name, String username, String password, String email, String phone) {
+        this.name = name;
         this.username = username;
+        this.email = email;
+        this.phone = phone;
         this.password = password;
     }
 
@@ -29,12 +35,36 @@ public class User {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getPassword() {
